e4c13bb8a692229c319345b24d162165d6057bf6
Tony DeVere <tdevere@microsoft.com>
2023-03-07 13:58:04 -0600

    AppResult[] results = app.WaitForElement(c => c.Marked("Hello World!"));

--- UITest1/Tests.cs
+++ UITest1/Tests.cs
@@ -28,7 +28,7 @@ namespace UITest1
         [Test]
         public void WelcomeTextIsDisplayed()
         {
-            AppResult[] results = app.WaitForElement(c => c.Marked("Welcome to Xamarin.Forms!"));
+            AppResult[] results = app.WaitForElement(c => c.Marked("Hello World!"));
             app.Screenshot("Welcome screen.");
 
             Assert.IsTrue(results.Any());
